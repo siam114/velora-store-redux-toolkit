6cbd4049652a9ba89f2a528a04e4e41d5d86bfbf
siam114 <smsiamislam598@gmail.com>
2025-05-22 20:27:10 +0600

    create index.css file

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <div>
-        <h2 className="text-4xl text-red-500 font-bold">Velora Store</h2>
+        <h2 className="text-4xl">Velora Store</h2>
     </div>
   )
 }
