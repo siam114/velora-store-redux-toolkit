56cc0352cd437e881f43c04c08faf0a5644057e4
siam114 <smsiamislam598@gmail.com>
2025-05-24 23:20:02 +0600

    change logo and creATE half navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import logo from '../../../public/logo.png'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className='bg-black p-2 w-full'>
+        <h3 className='text-white text-2xl font-bold tracking-normal leading-none text-center'>Welcome All</h3>
+      </div>
+
+      <div className='flex justify-around items-center'>
+        <div>
+            <img className='h-28 w-full' src={logo} alt="store" />
+        </div>
+        <div className='flex flex-row items-center '>
+            <button className='mr-4 text-2xl font-bold tracking-normal leading-none text-center'>Logout</button>
+        </div>
+      </div>
+    </>
   )
 }
 
