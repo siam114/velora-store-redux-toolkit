754cbb1fc47bdd416245d29fc782c7b2f18a46ef
siam114 <smsiamislam598@gmail.com>
2025-06-02 19:42:02 +0600

    create reducer and export reducer

--- src/feature/slices/sliderSlice.jsx
+++ src/feature/slices/sliderSlice.jsx
@@ -4,5 +4,14 @@ export const sliderSlice = createSlice({
     nane : 'slider',
     initialState: {
         value: 0,
+        length: 4,
     },
+    reducers: {
+        nextSlide() {},
+        prevSlide() {},
+        dotSlide() {},
+    }
 })
+
+export const { nextSlide, prevSlide, dotSlide } = sliderSlice.actions;
+export default sliderSlice.reducer;
