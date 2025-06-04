c1d5ec6ddf5fcfeb63bfcc79f047203b8e5c3df2
siam114 <smsiamislam598@gmail.com>
2025-06-04 19:51:20 +0600

    import next prev dot

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { nextSlide, prevSlide, dotSlide } from '../../feature/slices/sliderSlice'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Slider = () => {
   return (
     <div>
-      slider
+     <button>next</button>
+     <button>prev</button>
     </div>
   )
 }
